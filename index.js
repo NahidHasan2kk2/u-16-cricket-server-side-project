bbb465f152b8daf8cedf867d8ddfd280d332c583
nahidhasan2kk2 <nahidhasan2kk2@gmail.com>
2026-05-19 18:55:10 +0300

    added mongodb uri and confirm the connect

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+
 const express = require('express')
+const cors = require("cors");
+const dotenv = require('dotenv')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+dotenv.config();
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000;
+
+const uri = process.env.MONGODB_URL
+
+app.use(express.json());
+app.use(cors());
+
+
+
+const client = new MongoClient(uri, {
+ serverApi: {
+  version: ServerApiVersion.v1,
+  strict: true,
+  deprecationErrors: true,
+ }
+});
+
+
 
 app.get('/', (req, res) => {
  res.send('Hello World!')
 })
+
+
+async function run() {
+ try {
+  // Connect the client to the server	(optional starting in v4.7)
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+ } finally {
+
+  // await client.close();
+ }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
